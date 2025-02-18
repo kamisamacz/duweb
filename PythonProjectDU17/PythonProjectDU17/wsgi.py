efc85d3a131c0f0491d40983181a7cbb1a59059b
kamisamacz <kamisamacz@gmail.com>
2025-02-18 17:06:25 +0100

    Merge remote-tracking branch 'duweb/master'

--- PythonProjectDU17/PythonProjectDU17/wsgi.py
+++ PythonProjectDU17/PythonProjectDU17/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for PythonProjectDU17 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Nastavíme výchozí hodnotu pro DJANGO_SETTINGS_MODULE
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PythonProjectDU17.settings')
 
+# Získáme WSGI aplikaci
 application = get_wsgi_application()
